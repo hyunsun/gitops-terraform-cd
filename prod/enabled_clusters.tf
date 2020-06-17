395bc47fac6f00ee6c8d0ab48e7525b7170b4ecc
Hyunsun Moon <hyunsun.moon@gmail.com>
2020-06-17 01:12:59 -0700

    Remove Wisc Edge

--- prod/enabled_clusters.tf
+++ prod/enabled_clusters.tf
@@ -2,6 +2,5 @@ locals {
   enabled_clusters = {
     central = jsondecode(file("${path.module}/central/cluster.json"))
     edge_cloudlab_utah = jsondecode(file("${path.module}/edge-cloudlab-utah/cluster.json"))
-    edge_cloudlab_wisc = jsondecode(file("${path.module}/edge-cloudlab-wisc/cluster.json"))
   }
 }
