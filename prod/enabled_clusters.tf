22d727513c15df527be2e48ee28f3508debbf8d4
Hyunsun Moon <hyunsun.moon@gmail.com>
2020-06-17 09:45:11 -0700

    Add Wisc Edge

--- prod/enabled_clusters.tf
+++ prod/enabled_clusters.tf
@@ -2,5 +2,6 @@ locals {
   enabled_clusters = {
     central = jsondecode(file("${path.module}/central/cluster.json"))
     edge_cloudlab_utah = jsondecode(file("${path.module}/edge-cloudlab-utah/cluster.json"))
+    edge_cloudlab_wisc = jsondecode(file("${path.module}/edge-cloudlab-wisc/cluster.json"))
   }
 }
