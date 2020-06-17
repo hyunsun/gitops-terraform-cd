cf13a9c4f72973df6bd2bdc9f9b7ec20127e4fe0
Hyunsun Moon <hyunsun.moon@gmail.com>
2020-06-17 00:24:45 -0700

    Add utah edge

--- prod/enabled_clusters.tf
+++ prod/enabled_clusters.tf
@@ -1,6 +1,6 @@
 locals {
   enabled_clusters = {
     central = jsondecode(file("${path.module}/central/cluster.json"))
-    #edge_cloudlab_utah = jsondecode(file("${path.module}/edge-cloudlab-utah/cluster.json"))
+    edge_cloudlab_utah = jsondecode(file("${path.module}/edge-cloudlab-utah/cluster.json"))
   }
 }
